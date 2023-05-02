71fd5fb0537f94bf8c18d4b66250fcaf96d11824
Kimu-Nowchira <keleekini@gmail.com>
2023-05-03 01:54:16 +0900

    🎨 파일 정리

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,17 @@
-import { Module } from "@nestjs/common"
-import { AppController } from "./app.controller"
-import { AppService } from "./app.service"
-import { TypeOrmModule } from "@nestjs/typeorm"
-import { UsersModule } from "./users/users.module"
-import { NovelsModule } from "./novels/novels.module"
-import { BlocksModule } from "./blocks/blocks.module"
-import { EpisodesModule } from "./episodes/episodes.module"
-import { AuthModule } from "./auth/auth.module"
-import * as process from "process"
-import { ServeStaticModule } from "@nestjs/serve-static"
-import { join } from "path"
-import { PlaceController } from './place/place.controller';
-import { PlaceModule } from './place/place.module';
-import { FishController } from './fish/fish.controller';
-import { FacilitiesModule } from './facilities/facilities.module';
-import { FishModule } from './fish/fish.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "./users/users.module";
+import { AuthModule } from "./auth/auth.module";
+import * as process from "process";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
+import { FishController } from "./fish/fish.controller";
+import { FacilitiesModule } from "./facilities/facilities.module";
+import { FishModule } from "./fish/fish.module";
+import { PlacesModule } from "./places/places.module";
+import { PlacesController } from "./places/places.controller";
 
 @Module({
   imports: [
@@ -23,19 +20,16 @@ import { FishModule } from './fish/fish.module';
       url: process.env.DB_URL,
       autoLoadEntities: true,
     }),
-    UsersModule,
-    NovelsModule,
-    BlocksModule,
-    EpisodesModule,
-    AuthModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, "..", "..", "client", "dist"),
     }),
-    PlaceModule,
+    AuthModule,
+    UsersModule,
+    PlacesModule,
     FacilitiesModule,
     FishModule,
   ],
-  controllers: [AppController, PlaceController, FishController],
+  controllers: [AppController, PlacesController, FishController],
   providers: [AppService],
 })
 export class AppModule {}
