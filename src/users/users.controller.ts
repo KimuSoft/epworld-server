c223171f3a48859b832564eec4466d88af1e2aa2
Kimu-Nowchira <keleekini@gmail.com>
2023-07-31 04:50:26 +0900

    🔥refactor: remove unused api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,43 +1,21 @@
 import {
-  Body,
   Controller,
-  ForbiddenException,
   Get,
   NotFoundException,
+  NotImplementedException,
   Param,
-  Patch,
-  Post,
   Query,
   Request,
 } from "@nestjs/common"
 import { UsersService } from "./users.service"
-import { ApiOperation, ApiParam, ApiTags } from "@nestjs/swagger"
-import { Auth, User } from "../auth/auth.decorator"
-import { UserEntity } from "./user.entity"
+import { ApiOperation, ApiTags } from "@nestjs/swagger"
+import { Auth } from "../auth/auth.decorator"
 import { UserIdParamDto } from "./dto/user-id-param.dto"
-import { CreateUserDto } from "./dto/create-user.dto"
-import { UpdateUserDto } from "./dto/update-user.dto"
 
 @ApiTags("Users")
 @Controller("api/users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @ApiOperation({
-    summary: "유저 계정 생성",
-    description:
-      "관리자가 직접 계정을 생성하는 경우에만 사용한다. 일반적으로는 login을 통해 계정을 생성하며, 이 방식으로는 액세스 토큰을 얻을 수 없다.",
-  })
-  @Auth()
-  @Post(":id")
-  async createUser(
-    @User() user: UserEntity,
-    @Param() { id }: UserIdParamDto,
-    @Body() { username, avatar }: CreateUserDto
-  ) {
-    if (!user.admin) throw new ForbiddenException("You are not admin")
-    return this.usersService.create(id, username, avatar)
-  }
 
   // 토큰으로 세션 정보 얻기
   @ApiOperation({
@@ -57,26 +35,15 @@ export class UsersController {
   @Get(":id")
   async getUser(@Param() { id }: UserIdParamDto) {
     const user = await this.usersService.findById(id)
-    if (!user) throw new NotFoundException("User not found")
+    if (!user) throw new NotFoundException()
 
     return user
   }
 
-  @ApiOperation({
-    summary: "유저 정보 수정",
-    description: "해당 ID의 유저 정보를 수정한다. 본인 또는 관리자만 가능하다.",
-  })
-  @Patch(":id")
-  @Auth({ admin: true })
-  async updateUser(
-    @Param() { id }: UserIdParamDto,
-    @Body() { username, avatar }: UpdateUserDto
-  ) {
-    return this.usersService.update(id, username, avatar)
+  @Get(":id/dex")
+  async getUserDex(@Param("id") id: string) {
+    throw new NotImplementedException()
   }
-
-  // @Get(":id/dex")
-  // async getUserDex(@Param("id") id: string) {}
 
   @ApiOperation({
     summary: "유저가 낚았던 물고기 조회",
